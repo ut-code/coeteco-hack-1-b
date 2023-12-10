c1d5432f2a9336e149325129ca213af428f405cc
SatohiroToko <142480159+SatohiroToko@users.noreply.github.com>
2023-12-10 17:05:05 +0900

    update

--- flixflow-enhancer/src/App.js
+++ flixflow-enhancer/src/App.js
@@ -1,97 +1,78 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import "./App.css";
+import React, { useState, useRef } from "react"; 
+import { BrowserRouter as Router, Route, Link, useHistory } from "react-router-dom";
 
 const ImprovementSection = ({ number, teacherImage, studentImage, description }) => (
   <section className="improvement-section">
-    <h3>改善点{number}</h3>
+    <h3>改善点{number}</h3> 
     <div className="image-container">
       <img src={teacherImage} alt={`教師の画像${number}`} />
       <img src={studentImage} alt={`生徒の画像${number}`} />
     </div>
     <p>{description}</p>
-  </section>
+  </section>  
 );
 
 const ResultPage = () => (
-  <div className="result-container">
-    <h2 style={{ color: '#e74c3c' }}>改善点</h2>
-
-    <ImprovementSection
+  <div className="result-container">  
+    <h2 style={{color: "#e74c3c"}}>改善点</h2> 
+    <ImprovementSection 
       number={1}
       teacherImage="teacher_image_1.jpg"
-      studentImage="student_image_1.jpg"
-      description="このフレームでは、姿勢が安定していません。生徒さんはもう少し背筋を伸ばすように心がけましょう。"
+      studentImage="student_image_1.jpg" 
+      description="このフレームでは、姿勢が安定していません。生徒さんはもう少し背筋を伸ばすように心がけましょう。" 
     />
-
-    <ImprovementSection
+    <ImprovementSection  
       number={2}
       teacherImage="teacher_image_2.jpg"
-      studentImage="student_image_2.jpg"
-      description="手の位置が不安定です。教師のように手をしっかり支えて行うと良いでしょう。"
+      studentImage="student_image_2.jpg"  
+      description="手の位置が不安定です。教師のように手をしっかり支えて行うと良いでしょう。" 
     />
-
     {/* 同様に他の改善点を追加 */}
-
-    <button className="back-to-index">
-      <Link to="/">Index.html に戻る</Link>
+    
+    <button className="back-to-index">   
+      <Link to="/">Index.html に戻る</Link> 
     </button>
   </div>
 );
 
 const App = () => {
-  const [formData, setFormData] = useState({
+
+  const [formData, setFormData] = useState({  
     modelVideo: null,
     studentVideo: null,
-    tipsText: '',
+    tipsText: ""
   });
-
-  const updateFileName = (input, labelId) => {
-    const label = document.getElementById(labelId);
-    if (input.files.length > 0) {
-      label.textContent = input.files[0].name;
-    } else {
-      label.textContent = 'Choose File';
-    }
+  
+  const modelVideoInputRef = useRef(null);
+  const studentVideoInputRef = useRef(null); 
+  
+  const history = useHistory();
+  
+  const updateFileName = (inputRef, labelId) => {
+    // ファイル名を表示するロジック    
   };
 
   const submitForm = () => {
-    // フォーム送信のロジックを実装
+    // ここにフォーム送信のロジック
 
-    // フォーム送信が成功した場合
-    setFormData({
-      modelVideo: null,
-      studentVideo: null,
-      tipsText: '',
-    });
+    // 送信後にリダイレクト
+    history.push("/result");
   };
-
+  
   return (
     <Router>
       <Route path="/" exact>
         <div>
-          <header>
-            <div className="header-content">
-              <h1>FlipFlow-Enhancer</h1>
-              <p>小学生低学年の前転を改善するツール</p>
-            </div>
-          </header>
-
-          <main>
-            <form id="uploadForm">
-              {/* フォームの内容をここに記述 */}
-              <button type="button" onClick={submitForm}>
-                提出
-              </button>
-            </form>
-          </main>
+           {/* フォームの実装 */}
         </div>
       </Route>
 
-      <Route path="/result">
+      <Route path="/result" >  
         <ResultPage />
       </Route>
     </Router>
   );
-};
+}
 
 export default App;
